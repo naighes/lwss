d57a4884ea34135e4130eff86070ee70fa3fe7c2
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-02-17 08:20:34 +0100

    a bit of reorgnization.

--- cart/cart.js
+++ cart/cart.js
@@ -1,13 +1,16 @@
 'use strict';
 
 module.exports.create = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'it does nothing by now; it\'s just up \'n running.',
-      input: event,
-    }),
-  };
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'it does nothing by now; it\'s just up \'n running.',
+            input: event,
+        }),
+    };
 
-  callback(null, response);
+    callback(null, response);
 };
+
+
+
