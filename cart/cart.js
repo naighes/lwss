9545ddc412e2092ffa2a2f30c7bb4d95d6a212b4
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-02-19 10:50:23 +0100

    implementing some http facility.

--- cart/cart.js
+++ cart/cart.js
@@ -1,23 +1,25 @@
 'use strict';
+
 var fit = require('../lib/fluent-fit')
+var http = require('../lib/http')
+
+const invalidContent = () => {
+    return http.reply(400)
+        .jsonContent({
+            message: 'it looks you sent invalid content for application/json media type'
+        })
+        .getResponse()
+}
 
 module.exports.create = (event, context, callback) => {
     fit.perform(() => JSON.parse(event.body))
         .then((result) => {
-            callback(null, {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: 'it does nothing by now; it\'s just up \'n running.',
-                    input: event,
-                })
-            })
+            callback(null, http.reply(200)
+                .jsonContent({
+                    message: 'it does nothing by now; it\'s just up \'n running.'
+                }).getResponse())
         }, (error) => {
-            callback(null, {
-                statusCode: 400,
-                message: JSON.stringify({
-                    message: 'it looks you sent invalid content for application/json media type'
-                })
-            })
+            callback(null, invalidContent())
         })
 };
 
