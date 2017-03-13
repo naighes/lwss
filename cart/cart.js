92ed1c111b393a8aa379bb13fe71700acdec8b48
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-03-13 19:14:50 +0100

    db stuff within its own module.

--- cart/cart.js
+++ cart/cart.js
@@ -1,75 +1,12 @@
 'use strict'
 
 const http = require('../lib/http')
+const db = require('../lib/cart.db')
 const guid = require('../lib/guid')
-const AWS = require('aws-sdk')
-
-const paramsForCreate = (table, id, now) => {
-    return {
-        TableName : table,
-        Item: {
-            cart_id: id,
-            last_update: now(),
-            rows: { }
-        }
-    }
-}
-
-const paramsForDelete = (table, id) => {
-    return {
-        Key: { cart_id: id },
-        TableName : table
-    }
-}
-
-const paramsForAdd = (table, id, itemId, now, item) => {
-    return {
-        Key: { cart_id: id },
-        TableName: table,
-        UpdateExpression: 'SET #R.#item_id = :node, last_update = :last_update',
-        ExpressionAttributeNames: { '#item_id' : itemId, '#R': 'rows' },
-        ExpressionAttributeValues: {
-            ':last_update': now(),
-            ':node' : {
-                description: item.description,
-                price: item.price,
-                quantity: item.quantity,
-                thumb_url: item.thumb_url
-            }
-        }
-    }
-}
-
-const paramsForRemove = (table, id, itemId, now) => {
-    return {
-        Key: { cart_id: id },
-        TableName: table,
-        UpdateExpression: 'REMOVE #R.#item_id SET last_update = :last_update',
-        ExpressionAttributeNames: { '#item_id' : itemId, '#R': 'rows' },
-        ExpressionAttributeValues: {
-            ':last_update': now()
-        }
-    }
-}
-
-const paramsForGet = (table, id) => {
-    return {
-        TableName: table,
-        Key: { cart_id: id }
-    }
-}
 
 const baseUrl = (event) => {
     const scheme = event.headers['X-Forwarded-Proto']
     return `${scheme}://${event.headers.Host}/${event.requestContext.stage}/`
-}
-
-const now = () => {
-    return new Date().getTime()
-}
-
-const tableName = () => {
-    return process.env.CART_TABLE_NAME
 }
 
 const raiseError = (error) => {
@@ -88,74 +25,63 @@ const parseBody = (body, onSuccess, onError) => {
 }
 
 module.exports.create = (event, context, callback) => {
-    const db = new AWS.DynamoDB.DocumentClient()
     const id = guid.generate()
-    db.put(paramsForCreate(tableName(), id, now), (error, data) => {
-        if (error) {
-            raiseError(error).push(callback)
-        } else {
+    db.create(id)
+        .then(data => {
             http.reply(201)
                 .location(`${baseUrl(event)}carts/${id}`)
                 .enableCors()
                 .push(callback)
-        }
-    })
+        })
+        .catch(error => {
+            raiseError(error).push(callback)
+        })
 }
 
 module.exports.delete = (event, context, callback) => {
-    const db = new AWS.DynamoDB.DocumentClient()
-    db.delete(paramsForDelete(tableName(),
-        event.pathParameters.id),
-        (error, data) => {
-            if (error) {
-                raiseError(error).push(callback)
-            } else {
-                http.reply(204)
-                    .enableCors()
-                    .push(callback)
-            }
+    db.delete(event.pathParameters.id)
+        .then(data => {
+            http.reply(204)
+                .enableCors()
+                .push(callback)
+        })
+        .catch(error => {
+            raiseError(error).push(callback)
         })
 }
 
 module.exports.add = (event, context, callback) => {
-    const db = new AWS.DynamoDB.DocumentClient()
     parseBody(event.body,
-        (content) => {
-            db.update(paramsForAdd(tableName(),
-                event.pathParameters.id,
+        content => {
+            db.add(event.pathParameters.id,
                 event.pathParameters.item_id,
-                now,
-                content), (error, data) => {
-                    if (error) {
-                        raiseError(error).push(callback)
-                    } else {
-                        http.reply(204)
-                            .enableCors()
-                            .push(callback)
-                    }
+                content)
+                .then(data => {
+                    http.reply(204)
+                        .enableCors()
+                        .push(callback)
+                })
+                .catch(error => {
+                    raiseError(error).push(callback)
                 })
         },
-        (error) => { http.reply(400).enableCors().push(callback) })
+        error => { http.reply(400).enableCors().push(callback) })
 }
 
 module.exports.remove = (event, context, callback) => {
-    const db = new AWS.DynamoDB.DocumentClient()
-    db.update(paramsForRemove(tableName(),
-        event.pathParameters.id,
-        event.pathParameters.item_id,
-        now), (error, data) => {
-            if (error) {
-                raiseError(error).push(callback)
-            } else {
-                http.reply(204)
-                    .enableCors()
-                    .push(callback)
-            }
+    db.remove(event.pathParameters.id,
+        event.pathParameters.item_id)
+        .then(data => {
+            http.reply(204)
+                .enableCors()
+                .push(callback)
+        })
+        .catch(error => {
+            raiseError(error).push(callback)
         })
 }
 
 module.exports.get = (event, context, callback) => {
-    const db = new AWS.DynamoDB.DocumentClient()
     const handleResult = (data) => {
         if (Object.keys(data).length === 0) {
             http.reply(404)
@@ -172,13 +98,12 @@ module.exports.get = (event, context, callback) => {
         }
     }
 
-    db.get(paramsForGet(tableName(),
-        event.pathParameters.id), (error, data) => {
-            if (error) {
-                raiseError(error).push(callback)
-            } else {
-                handleResult(data)
-            }
+    db.get(event.pathParameters.id)
+        .then(data => {
+            handleResult(data)
+        })
+        .catch(error => {
+            raiseError(error).push(callback)
         })
 }
 
