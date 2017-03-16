3792fd4efab96b3f6f43501d8e812c992918e78e
nicola baldi (@naighes) <nic.baldi@gmail.com>
2017-03-16 19:12:40 +0100

    removing some boilerplate code.

--- cart/cart.js
+++ cart/cart.js
@@ -3,6 +3,7 @@
 const http = require('../lib/http')
 const cart = require('../lib/cart.db')
 const guid = require('../lib/guid')
+const utils = require('../lib/utils')
 
 const baseUrl = event => {
     const scheme = event.headers['X-Forwarded-Proto']
@@ -12,14 +13,16 @@ const baseUrl = event => {
 const raiseError = error => http.reply(500)
     .enableCors()
     .jsonContent({ message: 'oh my...', error: error })
+    .push
 
 const raiseBadRequest = error => http.reply(400)
     .enableCors()
     .jsonContent({ error: error })
+    .push
 
-const raiseNoContent = () => http.reply(204).enableCors()
+const raiseNoContent = () => http.reply(204).enableCors().push
 
-const raiseNotFound = () => http.reply(404).enableCors()
+const raiseNotFound = () => http.reply(404).enableCors().push
 
 const parseBody = (body, onSuccess, onError) => {
     try {
@@ -37,47 +40,40 @@ module.exports.create = (event, context, callback) => {
             .location(`${baseUrl(event)}carts/${id}`)
             .enableCors()
             .push(callback))
-        .catch(error => raiseError(error).push(callback))
+        .catch(error => raiseError(error)(callback))
 }
 
 // TODO: return 404 on item not found
 module.exports.delete = (event, context, callback) =>
     cart.delete(event.pathParameters.id)
-        .then(data => raiseNoContent().push(callback))
-        .catch(error => raiseError(error).push(callback))
+        .then(data => raiseNoContent()(callback))
+        .catch(error => raiseError(error)(callback))
 
 module.exports.add = (event, context, callback) => {
-    const handleResult = data => {
-        if (typeof(data) === 'undefined' || Object.keys(data).length === 0) {
-            // TODO: add location
-            return http.reply(201)
-                .enableCors()
-                .push
-        }
-
-        return raiseNoContent().push
-    }
+    const handleResult = data => utils.emptyOrUndefined(data)
+        ? http.reply(201).enableCors().push // TODO: add location
+        : raiseNoContent()
     parseBody(event.body,
         content => {
             content.id = event.pathParameters.item_id
             cart.add(event.pathParameters.id, content)
                 .then(data => handleResult(data)(callback))
-                .catch(error => raiseError(error).push(callback))
+                .catch(error => raiseError(error)(callback))
         },
-        error => raiseBadRequest(error).push(callback))
+        error => raiseBadRequest(error)(callback))
 }
 
 // TODO: return 404 on item not found
 module.exports.remove = (event, context, callback) =>
     cart.remove(event.pathParameters.id,
         event.pathParameters.item_id)
-        .then(data => raiseNoContent().push(callback))
-        .catch(error => raiseError(error).push(callback))
+        .then(data => raiseNoContent()(callback))
+        .catch(error => raiseError(error)(callback))
 
 module.exports.get = (event, context, callback) => {
     const handleResult = data => {
         if (Object.keys(data).length === 0) {
-            raiseNotFound().push(callback)
+            raiseNotFound()(callback)
         } else {
             http.reply(200)
                 .lastModified(new Date(data.last_update))
@@ -89,7 +85,6 @@ module.exports.get = (event, context, callback) => {
 
     cart.get(event.pathParameters.id)
         .then(data => handleResult(data))
-        .catch(error => raiseError(error)
-            .push(callback))
+        .catch(error => raiseError(error)(callback))
 }
 
